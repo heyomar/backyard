f75c1437d7fbad3e8106f17f5669850f66241a1a
heyomar <25757036+heyomar@users.noreply.github.com>
2022-01-02 18:42:54 -0600

    delete and check todo

--- javascript/todo/main.js
+++ javascript/todo/main.js
@@ -9,21 +9,49 @@ let todo_input_field = document.querySelector('.todo_input_field')
 
 // Event Listeners
 todo_submit_btn.addEventListener('click', addTodo)
+todo_list.addEventListener('click', buttonActions)
 
 // Functions
 function addTodo(event) {
     event.preventDefault()
 
-    const list_item_element = document.createElement('li')
+    if (todo_input_field.value === '' || null || undefined) {
+        return
+    }
 
-    list_item_element.classList.add('todo_item')
-    list_item_element.innerText = todo_input_field.value
+    const todo_item = document.createElement('li')
 
-    todo_list.appendChild(list_item_element)
+    todo_item.classList.add('todo_item')
+    todo_item.innerText = todo_input_field.value
+
+    todo_list.appendChild(todo_item)
 
     todo_input_field.value = ''
+
+    // Add buttons
+    const check_btn = document.createElement('button')
+    check_btn.classList.add('btn_check')
+    check_btn.innerHTML = '<i class="lni lni-checkmark-circle"></i>'
+    todo_item.appendChild(check_btn)
+
+    const delete_btn = document.createElement('button')
+    delete_btn.classList.add('btn_trash')
+    delete_btn.innerHTML = '<i class="lni lni-trash-can"></i>'
+    todo_item.appendChild(delete_btn)
+
 }
 
+function buttonActions(event) {
+    let current_todo = event.target.parentElement
 
+    if (event.target.classList.contains('btn_trash')) {
+        current_todo.remove()
+    }
+
+    if (event.target.classList.contains('btn_check')) {
+        current_todo.classList.toggle('line-through')
+        current_todo.classList.toggle('opacity-50')
+    }
+}
 
 
